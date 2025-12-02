34566d24a65433325ede2b94d4a821f35f90486c
Erick Vinicius Rodrigues <erickafram08@gmail.com>
2025-12-02 08:59:19 -0300

    Fix: Corrigir middleware legacyRedirect para não bloquear URLs AMP válidas

--- test-insta.js
+++
@@ -1,9 +0,0 @@
-const insta = require("instagram-url-direct");
-console.log('Type:', typeof insta);
-console.log('Value:', insta);
-
-if (typeof insta === 'function') {
-    console.log('It is a function');
-} else {
-    console.log('Keys:', Object.keys(insta));
-}
